5c4152477daa482f93d9d4e585ae50acb54c9827
arturantunes <aas4@cesar.school>
2025-04-06 17:23:18 -0300

    adiciona classe pelada

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Pelada(models.Model):
+    nome = models.CharField(max_length=100)
+    data = models.DateField()
+    local = models.CharField(max_length=100)
