932e0a104b56701e075c0177998673296605e0c6
arturantunes <aas4@cesar.school>
2025-04-06 17:24:25 -0300

    adiciona classe presenca

--- core/models.py
+++ core/models.py
@@ -10,3 +10,8 @@ class Jogador(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     posicao = models.CharField(max_length=50)
+
+class Presenca(models.Model):
+    jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE)
+    pelada = models.ForeignKey(Pelada, on_delete=models.CASCADE)
+    confirmado = models.BooleanField(default=False)
