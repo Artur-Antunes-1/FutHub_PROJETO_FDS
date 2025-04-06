36f48f913fe7c415d16fd583315cd5355333df52
arturantunes <aas4@cesar.school>
2025-04-06 18:09:57 -0300

    Corrige erro de importação do forum

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
+    # path('forum/', include('forum.urls')),
     path('', include('core.urls')),
 ]
