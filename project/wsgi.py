4277eee15799096f59635fee8142af35515a6954
arturantunes <aas4@cesar.school>
2025-04-28 09:34:38 -0300

    ajusta wsgi.py para deploy

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django.setup()
+
+# Executa as migrations antes de criar o application
+call_command("migrate", interactive=False)
 
 application = get_wsgi_application()
+
