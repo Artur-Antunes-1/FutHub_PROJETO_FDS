7dc8586c248aa26949cac30fd0da0a86f7dd739a
arturantunes <aas4@cesar.school>
2025-05-27 18:41:57 -0300

    prepara arquivo yml para deploy com testes em cypress

--- tests/e2e/test_register.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from tests.e2e.pages.auth import AuthPage
-
-@pytest.mark.asyncio
-async def test_register(live_server, async_page):
-    auth = AuthPage(async_page, live_server.url)
-    await auth.register("novo_user", "segredo123")
-    await async_page.wait_for_url("**/")          # a home é “/”
-    assert await async_page.is_visible("text=Bem-vindo")
